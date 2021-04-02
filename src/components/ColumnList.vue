96a872c5ef7bae896a307aa6bb8b354d72579ee5
jay chou <jaychou@email.com>
2021-04-02 21:05:02 +0800

    elementplus

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="6" v-for="column in list" :key="column.id">
+      <img :src="column.avatar" :alt="column.title">
+      <h5>{{column.title}}</h5>
+      <p>{{column.description}}</p>
+      <a href="#">进入专栏</a>
+    </el-col>
+  </el-row>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import { ElRow, ElCol, ElButton, ElSelect } from 'element-plus'
+import 'element-plus/packages/theme-chalk/src/base.scss'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component(ElButton.name, ElButton)
+app.component(ElSelect.name, ElSelect)
+app.component(ElRow.name, ElRow)
+app.component(ElCol.name, ElCol)
+app.mount('#app')
